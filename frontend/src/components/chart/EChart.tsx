155be939073fb7fea1af117701ddbe2927f0df68
thav2003 <vuthase172485@fpt.edu.vn>
2024-06-29 06:41:26 +0700

    fix dashboard

---
+++ frontend/src/components/chart/EChart.tsx
@@ -0,0 +1,33 @@
+/*!
+  =========================================================
+  * Muse Ant Design Dashboard - v1.0.0
+  =========================================================
+  * Product Page: https://www.creative-tim.com/product/muse-ant-design-dashboard
+  * Copyright 2021 Creative Tim (https://www.creative-tim.com)
+  * Licensed under MIT (https://github.com/creativetimofficial/muse-ant-design-dashboard/blob/main/LICENSE.md)
+  * Coded by Creative Tim
+  =========================================================
+  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+import ReactApexChart from 'react-apexcharts'
+import { Row, Col, Typography } from 'antd'
+import eChart from './configs/eChart'
+
+const EChart: React.FC<{ data?: any }> = ({ data }) => {
+  return (
+    <>
+      <div id='chart'>
+        <ReactApexChart
+          className='bar-chart'
+          options={eChart.options}
+          series={data ? data : eChart.series}
+          type='bar'
+          height={220}
+        />
+      </div>
+    </>
+  )
+}
+
+export default EChart
---
+++ frontend/src/components/chart/configs/eChart.ts
@@ -0,0 +1,78 @@
+const eChart = {
+  series: [
+    {
+      name: 'Sales',
+      data: [450, 200, 100, 220, 500, 100, 400],
+      color: '#fff'
+    }
+  ],
+
+  options: {
+    chart: {
+      type: 'bar',
+      width: '100%',
+      height: 'auto',
+
+      toolbar: {
+        show: false
+      }
+    },
+    plotOptions: {
+      bar: {
+        horizontal: false,
+        columnWidth: '55%',
+        borderRadius: 5
+      }
+    },
+    dataLabels: {
+      enabled: false
+    },
+    stroke: {
+      show: true,
+      width: 1,
+      colors: ['transparent']
+    },
+    grid: {
+      show: true,
+      borderColor: '#ccc',
+      strokeDashArray: 2
+    },
+    xaxis: {
+      categories: ['Thứ 2', 'Thứ 3', 'Thứ 4', 'Thứ 5', 'Thứ 6', 'Thứ 7', 'Chủ nhật'],
+      labels: {
+        show: true,
+        align: 'right',
+        minWidth: 0,
+        maxWidth: 160,
+        style: {
+          colors: ['#fff', '#fff', '#fff', '#fff', '#fff', '#fff', '#fff']
+        }
+      }
+    },
+    yaxis: {
+      labels: {
+        show: true,
+        align: 'right',
+        minWidth: 0,
+        maxWidth: 160,
+        style: {
+          colors: ['#fff', '#fff', '#fff', '#fff', '#fff', '#fff', '#fff']
+        },
+
+        formatter: function (value) {
+          return Math.floor(value) // Chuyển đổi các giá trị thành số nguyên
+        }
+      }
+    },
+
+    tooltip: {
+      y: {
+        formatter: function (val) {
+          return val + ' Người'
+        }
+      }
+    }
+  }
+}
+
+export default eChart
