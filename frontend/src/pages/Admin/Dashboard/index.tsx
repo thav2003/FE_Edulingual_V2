f7421cc8d12a3179eb87e1c201b5155e0e3f6a78
thav2003 <vuthase172485@fpt.edu.vn>
2024-06-30 16:24:07 +0700

    fix api dashboard

--- frontend/src/pages/Admin/Dashboard/index.tsx
+++ frontend/src/pages/Admin/Dashboard/index.tsx
@@ -1,218 +1,3 @@
-// import React from 'react'
-// import type { StatisticProps } from 'antd'
-// import { Card, Col, ConfigProvider, Row, Space, Statistic, Typography } from 'antd'
-// import CountUp from 'react-countup'
-// import { Column, DualAxes } from '@ant-design/plots'
-// import { Line } from '@ant-design/plots'
-// import useFetchData from '~/hooks/useFetch'
-// import { DashboardApi } from '~/api'
-// import { formatCurrencyVND } from '~/utils/numberUtils'
-// import { randomInt } from 'crypto'
-// const formatter: StatisticProps['formatter'] = (value) => <CountUp end={value as number} separator=',' />
-
-// const dashboardApi = new DashboardApi()
-// const DashboardPage: React.FC = () => {
-//   const configLine = {
-//     data: {
-//       type: 'fetch',
-//       value: 'https://assets.antv.antgroup.com/g2/indices.json'
-//     },
-//     xField: (d) => new Date(d.Date),
-//     yField: 'Close',
-//     colorField: 'Symbol',
-//     normalize: { basis: 'first', groupBy: 'color' },
-//     scale: {
-//       y: { type: 'log' }
-//     },
-//     axis: {
-//       y: { title: '↑ Change in price (%)' }
-//     },
-//     label: {
-//       text: 'Symbol',
-//       selector: 'last',
-//       style: {
-//         fontSize: 10
-//       }
-//     },
-//     tooltip: { channel: 'y', valueFormatter: '.1f' }
-//   }
-
-//   const fetchUsers = () => {
-//     return dashboardApi.apiV1DashboardUserGet()
-//   }
-//   const fetchTeachers = () => {
-//     return dashboardApi.apiV1DashboardTeacherGet()
-//   }
-//   // const fetchExams = () => {
-//   //   return dashboardApi.apiV1DashboardExamGet()
-//   // }
-//   const fetchFinances = () => {
-//     return dashboardApi.apiV1DashboardFinanceGet()
-//   }
-//   const [loadingUsers, errorUsers, responseUsers] = useFetchData(fetchUsers)
-//   const [loadingTeachers, errorTeachers, responseTeachers] = useFetchData(fetchTeachers)
-//   // const [loadingExams, errorExams, responseExams] = useFetchData(fetchExams)
-//   const [loadingFinances, errorFinances, responseFinances] = useFetchData(fetchFinances)
-
-//   console.log(responseFinances?.data?.data)
-//   const dataFinances = responseFinances ? responseFinances?.data?.data : {}
-//   const dataTeachers = responseTeachers ? responseTeachers?.data?.data : {}
-//   const dataUsers = responseUsers ? responseUsers?.data?.data : {}
-//   console.log(dataFinances)
-
-//   const dayMap = {
-//     dataInMonday: 'Thứ 2',
-//     dataInTuesday: 'Thứ 3',
-//     dataInWednesday: 'Thứ 4',
-//     dataInThursday: 'Thứ 5',
-//     dataInFriday: 'Thứ 6',
-//     dataInSaturday: 'Thứ 7',
-//     dataInSunday: 'Chủ nhật'
-//   }
-//   const resultFinance = Object.entries(dataFinances).map(([key, value]) => ({
-//     day: dayMap[key],
-//     value: value
-//   }))
-//   const resultTeachers = Object.entries(dataTeachers).map(([key, value]) => ({
-//     day: dayMap[key],
-//     value: value
-//   }))
-//   const resultUsers = Object.entries(dataUsers).map(([key, value]) => ({
-//     day: dayMap[key],
-//     value: value
-//   }))
-//   return (
-//     <div
-//       style={{
-//         height: '100%'
-//       }}
-//     >
-//       <ConfigProvider
-//         theme={{
-//           token: {
-//             lineWidth: 1
-//           }
-//         }}
-//       >
-//         <div className='h-full px-10 bg-[#FFFFFF]'>
-//           <Row gutter={[32, 32]}>
-//             <Col span={24}>
-//               <Typography.Title>Teacher</Typography.Title>
-//               <Card bordered>
-//                 <Column
-//                   {...{
-//                     height: 400,
-//                     data: resultTeachers,
-//                     xField: 'day',
-//                     yField: 'value',
-//                     label: {
-//                       text: (d) => `${d.value} người`,
-//                       textBaseline: 'bottom'
-//                     },
-//                     tooltip: {
-//                       title: 'Người',
-
-//                       items: [{ channel: 'y', field: 'value', name: 'Người' }]
-//                     },
-//                     axis: {
-//                       y: {
-//                         labelFormatter: (v) => {
-//                           {
-//                             if (v % 1 === 0) {
-//                               return `${v} Người`
-//                             } else {
-//                               return ''
-//                             }
-//                           }
-//                         }
-//                       }
-//                     },
-//                     legend: false
-//                   }}
-//                 />
-//               </Card>
-//             </Col>
-//             <Col span={24}>
-//               <Typography.Title>User</Typography.Title>
-//               <Card bordered>
-//                 <Column
-//                   {...{
-//                     height: 400,
-//                     data: resultUsers,
-//                     xField: 'day',
-//                     yField: 'value',
-//                     label: {
-//                       text: (d) => `${d.value} người`,
-//                       textBaseline: 'bottom'
-//                     },
-//                     tooltip: {
-//                       title: 'Người',
-
-//                       items: [{ channel: 'y', field: 'value', name: 'Người' }]
-//                     },
-//                     axis: {
-//                       y: {
-//                         labelFormatter: (v) => {
-//                           {
-//                             if (v % 1 === 0) {
-//                               return `${v} Người`
-//                             } else {
-//                               return ''
-//                             }
-//                           }
-//                         }
-//                       }
-//                     },
-//                     legend: false
-//                   }}
-//                 />
-//               </Card>
-//             </Col>
-//             <Col span={24}>
-//               <Typography.Title>Finance</Typography.Title>
-//               <Card bordered>
-//                 <Column
-//                   {...{
-//                     height: 400,
-//                     data: resultFinance,
-//                     xField: 'day',
-//                     yField: 'value',
-//                     label: {
-//                       text: (d) => `${formatCurrencyVND(d.value)}`,
-//                       textBaseline: 'bottom'
-//                     },
-//                     axis: {
-//                       y: {
-//                         labelFormatter: (v) => {
-//                           {
-//                             if (v % 1 === 0) {
-//                               return `${v} VNĐ`
-//                             } else {
-//                               return ''
-//                             }
-//                           }
-//                         }
-//                       }
-//                     },
-//                     tooltip: {
-//                       title: 'VNĐ',
-
-//                       items: [{ channel: 'y', field: 'value', name: 'VNĐ' }]
-//                     },
-//                     legend: false
-//                   }}
-//                 />
-//               </Card>
-//             </Col>
-//           </Row>
-//         </div>
-//       </ConfigProvider>
-//     </div>
-//   )
-// }
-
-// export default DashboardPage
-
 import { useState } from 'react'
 
 import { Card, Col, Row, Typography } from 'antd'
@@ -233,15 +18,24 @@ const Dashboard: React.FC = () => {
   const fetchFinances = () => {
     return dashboardApi.apiV1DashboardFinanceGet()
   }
+  const fetchMonthFinances = () => {
+    return dashboardApi.apiV1DashboardFinanceByMonthGet()
+  }
+  const fetchDashboard = () => {
+    return dashboardApi.apiV1DashboardGet()
+  }
   const [loadingUsers, errorUsers, responseUsers] = useFetchData(fetchUsers)
   const [loadingTeachers, errorTeachers, responseTeachers] = useFetchData(fetchTeachers)
   const [loadingFinances, errorFinances, responseFinances] = useFetchData(fetchFinances)
+  const [loadingMonthFinances, errorMonthFinances, responseMonthFinances] = useFetchData(fetchMonthFinances)
+  const [loadingDashboard, errorDashboard, responseDashboard] = useFetchData(fetchDashboard)
 
   console.log(responseFinances?.data?.data)
   const dataFinances = responseFinances ? responseFinances?.data?.data : {}
   const dataTeachers = responseTeachers ? responseTeachers?.data?.data : {}
   const dataUsers = responseUsers ? responseUsers?.data?.data : {}
-  console.log(dataFinances)
+  const dataMonthFinances = responseMonthFinances ? responseMonthFinances?.data?.data : {}
+  console.log(responseDashboard?.data.data)
 
   const dayMap = {
     dataInMonday: 'Thứ 2',
@@ -252,8 +46,26 @@ const Dashboard: React.FC = () => {
     dataInSaturday: 'Thứ 7',
     dataInSunday: 'Chủ nhật'
   }
+  const monthMap = {
+    dataInJanuary: 'Tháng 1',
+    dataInFebruary: 'Tháng 2',
+    dataInMarch: 'Tháng 3',
+    dataInApril: 'Tháng 4',
+    dataInMay: 'Tháng 5',
+    dataInJune: 'Tháng 6',
+    dataInJuly: 'Tháng 7',
+    dataInAugust: 'Tháng 8',
+    dataInSeptember: 'Tháng 9',
+    dataInOctober: 'Tháng 10',
+    dataInNovember: 'Tháng 11',
+    dataInDecember: 'Tháng 12'
+  }
   const resultFinance = Object.entries(dataFinances).map(([key, value]) => ({
     day: dayMap[key],
+    value: value
+  }))
+  const resultMonthFinance = Object.entries(dataMonthFinances).map(([key, value]) => ({
+    day: monthMap[key],
     value: value
   }))
   const resultTeachers = Object.entries(dataTeachers).map(([key, value]) => ({
@@ -267,6 +79,7 @@ const Dashboard: React.FC = () => {
   console.log(resultUsers)
   console.log(resultTeachers)
   console.log(resultFinance)
+  console.log(resultMonthFinance)
 
   const dollor = [
     <svg width='22' height='22' viewBox='0 0 20 20' fill='none' xmlns='http://www.w3.org/2000/svg' key={0}>
@@ -326,20 +139,20 @@ const Dashboard: React.FC = () => {
   const count = [
     {
       today: 'Doanh thu hôm nay',
-      title: '53,000',
+      title: responseDashboard?.data.data.revenue,
       //{formatCurrencyVND()}
       icon: dollor,
       bnb: 'bnb2'
     },
     {
-      today: 'Giao dịch hôm nay',
-      title: '3,200',
+      today: 'Lợi nhuận hôm nay',
+      title: responseDashboard?.data.data.profit,
       icon: profile,
       bnb: 'bnb2'
     },
     {
-      today: 'Hoa hồng hôm nay',
-      title: '+1,200',
+      today: 'Tổng tiền hôm nay',
+      title: responseDashboard?.data.data.totalPayments,
       icon: heart,
       bnb: 'redtext'
     }
@@ -392,7 +205,7 @@ const Dashboard: React.FC = () => {
                 data={[
                   {
                     name: 'VNĐ',
-                    data: resultFinance.map((e) => e.value),
+                    data: resultMonthFinance.map((e) => e.value),
                     color: '#fff'
                   }
                 ]}
